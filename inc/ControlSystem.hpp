4976446ff0a9bdf16b84e78552f88a3a2d9d2198
Mathias Schuler <mathiasschuler96@gmail.com>
2023-05-16 13:53:33 +0000

    Added steer-by-wire with subsystems

--- inc/ControlSystem.hpp
+++ inc/ControlSystem.hpp
@@ -4,12 +4,10 @@
 #include <eeros/control/TimeDomain.hpp>
 #include <eeros/core/Executor.hpp>
 #include <eeros/control/PeripheralInput.hpp>
-#include <eeros/control/Sum.hpp>
 #include <eeros/control/Mux.hpp>
+#include "customBlocks/Controller.hpp"
+#include "customBlocks/InvMotMod.hpp"
 #include <eeros/control/DeMux.hpp>
-#include <eeros/control/Gain.hpp>
-#include <eeros/control/D.hpp>
-#include <eeros/control/Saturation.hpp>
 #include <eeros/control/PeripheralOutput.hpp>
 
 using namespace eeros::control;
@@ -23,21 +21,8 @@ public:
     PeripheralInput<> E1, E2;
     Mux<2> E;
     Mux<2> E_d;
-    Sum<2, eeros::math::Vector2> e;
-    Gain<eeros::math::Vector2> Kp;
-    D<eeros::math::Vector2> ed;
-    Gain<eeros::math::Vector2> Kd;
-    Sum<2, eeros::math::Vector2> qdd_c;
-    Gain<eeros::math::Vector2> M;
-    Saturation<eeros::math::Vector2> QMax;
-    Gain<eeros::math::Vector2> iInv;
-    Gain<eeros::math::Vector2> kMInv;
-    Gain<eeros::math::Vector2> R;
-    D<eeros::math::Vector2> qd1;
-    Saturation<eeros::math::Vector2> qdMax;
-    Gain<eeros::math::Vector2> i;
-    Gain<eeros::math::Vector2> kM;
-    Sum<2, eeros::math::Vector2> U1;
+    Controller<eeros::math::Vector2> cont;
+    InvMotMod<eeros::math::Vector2> invMotMod;
     DeMux<2> U;
     PeripheralOutput<> M1;
     PeripheralOutput<> M2;
